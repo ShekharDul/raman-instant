a02c16092a21a16537b040ae653d66c25a9a14f3
unknown <shekhardulgach19@gmail.com>
2026-04-28 01:14:01 +0530

    feat: add technical protocol documentation page

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        docs: './docs.html'
+      }
+    }
   }
 });
